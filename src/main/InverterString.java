dbea38dd5c6871737579376130ec1f4db9db4d8b
zulang21 <arthurricken1@gmail.com>
2024-11-04 00:07:01 -0300

    Consegui

--- src/main/InverterString.java
+++ src/main/InverterString.java
@@ -3,17 +3,17 @@ package main;
 public class InverterString {
     public static void main(String[] args) {
         String palavra = "oval";
-        StringBuilder palavra2 = new StringBuilder(palavra);
-        String inverter = inverter(palavra, palavra2, 0, palavra.length() - 1);
-        System.out.println(inverter);
+        inverter(palavra,0,palavra.length()-1);
+        
     }
-
-    public static String inverter(String palavra, StringBuilder palavra2, int inicio, int fim) { 
-        if(inicio>palavra.length() && fim<0){
-            return "";
-        }else{
-        palavra2.setCharAt(fim, palavra.charAt(inicio));
-        return inverter(palavra, palavra2, inicio+1, fim-1);
-        }   
+    public static String inverter(String palavra, int inicio,int fim) { 
+        char[] letras = palavra.toCharArray();
+        char[] letrasInvertidas = new char[letras.length];
+        if (inicio<letras.length && fim>=0) {
+            letrasInvertidas[inicio]=letras[fim];
+            System.out.print(letrasInvertidas[inicio]);
+            return inverter(palavra,inicio+1,fim-1);
+        }
+        return "";
 }
 }
